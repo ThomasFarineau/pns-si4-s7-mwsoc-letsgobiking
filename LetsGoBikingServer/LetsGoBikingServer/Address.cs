99a94c0b0ccb619bba08946910dc629d178ac942
Thomas <thomas.farineau@icloud.com>
2022-11-21 10:15:29 +0100

    add Address.cs

---
+++ LetsGoBikingServer/LetsGoBikingServer/Address.cs
@@ -0,0 +1,16 @@
+﻿using System.Device.Location;
+
+namespace LetsGoBikingServer
+{
+    public class Address
+    {
+        private string city;
+        private GeoCoordinate geoCoordinate;
+        
+        public Address(string city, GeoCoordinate geoCoordinate)
+        {
+            this.city = city;
+            this.geoCoordinate = geoCoordinate;
+        }
+    }
+}
