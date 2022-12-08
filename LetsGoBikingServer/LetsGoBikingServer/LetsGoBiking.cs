bca81748f1a4f1d7e5e5f4f7c0619e3c85518ddc
Léo K <leo.kitabdjian@etu.unice.fr>
2022-12-08 12:09:48 +0100

    ActiveMQ

--- LetsGoBikingServer/LetsGoBikingServer/LetsGoBiking.cs
+++ LetsGoBikingServer/LetsGoBikingServer/LetsGoBiking.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Device.Location;
 using System.Linq;
 using System.Net.Http;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
 
 namespace LetsGoBikingServer;
 
@@ -19,6 +21,7 @@ public class LetsGoBiking : ILetsGoBiking
         var o = CallOrsSearchApi(origin);
         var d = CallOrsSearchApi(destination);
         var instructions = GetInstructions(o, d);
+        HandleActiveMQ(instructions);
         return instructions;
     }
 
@@ -90,5 +93,45 @@ public class LetsGoBiking : ILetsGoBiking
         var proxyClient = new ProxyServiceClient();
         Station closest = proxyClient.ClosestStation(geoCoordinate);
         return closest.Coordinate;
+    }
+
+    public void HandleActiveMQ(string[] instructions)
+    {
+        // Create a Connection Factory.
+        Uri connecturi = new Uri("activemq:tcp://localhost:61616");
+        ConnectionFactory connectionFactory = new ConnectionFactory(connecturi);
+
+        // Create a single Connection from the Connection Factory.
+        IConnection connection = connectionFactory.CreateConnection();
+        connection.Start();
+
+        // Create a session from the Connection.
+        ISession session = connection.CreateSession();
+
+        // Use the session to target a queue.
+        IDestination destination = session.GetQueue("instructions");
+
+        // Create a Producer targetting the selected queue.
+        IMessageProducer producer = session.CreateProducer(destination);
+
+        // You may configure everything to your needs, for instance:
+        producer.DeliveryMode = MsgDeliveryMode.NonPersistent;
+
+        // Finally, to send messages:
+        //ITextMessage message = session.CreateTextMessage("Hello World 2");
+        //producer.Send(message);
+
+        foreach (string i in instructions)
+        {
+            ITextMessage message = session.CreateTextMessage(i);
+            producer.Send(message);
+        }
+
+        Console.WriteLine("Message sent, check ActiveMQ web interface to confirm.");
+        Console.ReadLine();
+
+        // Don't forget to close your session and connection when finished.
+        session.Close();
+        connection.Close();
     }
 }
