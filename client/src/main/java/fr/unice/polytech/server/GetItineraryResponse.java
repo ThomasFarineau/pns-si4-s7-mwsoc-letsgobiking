019598ed6cc55c84e8a7fa11653ca70f65878cc1
Léo K <leo.kitabdjian@etu.unice.fr>
2022-12-08 13:03:40 +0100

    jaxws imports

---
+++ client/src/main/java/fr/unice/polytech/server/GetItineraryResponse.java
@@ -0,0 +1,65 @@
+
+package fr.unice.polytech.server;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElementRef;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour anonymous complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="GetItineraryResult" type="{http://schemas.microsoft.com/2003/10/Serialization/Arrays}ArrayOfstring" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "getItineraryResult"
+})
+@XmlRootElement(name = "GetItineraryResponse")
+public class GetItineraryResponse {
+
+    @XmlElementRef(name = "GetItineraryResult", namespace = "http://tempuri.org/", type = JAXBElement.class, required = false)
+    protected JAXBElement<ArrayOfstring> getItineraryResult;
+
+    /**
+     * Obtient la valeur de la propriété getItineraryResult.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link ArrayOfstring }{@code >}
+     *     
+     */
+    public JAXBElement<ArrayOfstring> getGetItineraryResult() {
+        return getItineraryResult;
+    }
+
+    /**
+     * Définit la valeur de la propriété getItineraryResult.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link ArrayOfstring }{@code >}
+     *     
+     */
+    public void setGetItineraryResult(JAXBElement<ArrayOfstring> value) {
+        this.getItineraryResult = value;
+    }
+
+}
